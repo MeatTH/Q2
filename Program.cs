a454563f623b27e25a7c17ded4482324877e9bc2
MeatTH <122563787+MeatTH@users.noreply.github.com>
2023-03-29 14:21:52 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+    class program {
+        static void Main(string[] args) {
+
+        Console.WriteLine("N:");
+        int N = int.Parse(Console.ReadLine());
+        Console.WriteLine("K:");
+        int K = int.Parse(Console.ReadLine());
+        
+        int N,K;
+
+        for (int adder = 1; adder <= N;adder++){
+          Console.WriteLine("S:");
+          int S = int.Parse(Console.ReadLine());
+          Console.WriteLine("S1");
+          int S1 = int.Parse(Console.ReadLine());
+          Console.WriteLine("S2");
+          int S2 = int.Parse(Console.ReadLine());
+
+        }
+      }
+    }
+
+    
+        
+                  
+               
+        
+    
